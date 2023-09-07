eea49343a480c8e96962441c23cc8e3c7856545d
BohdanLytvynov <blitvinov19@gmail.com>
2023-09-07 20:45:34 +0300

    Task 4 finished

---
+++ Task4/Task4.cpp
@@ -0,0 +1,20 @@
+﻿// Task4.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include <windows.h>
+
+
+int main()
+{
+    wchar_t const* myTitle{ L"My title!" };
+
+    SetConsoleTitle(myTitle);
+
+    std::cout << "Change console's header!\n";
+
+    system("pause");
+
+    return 0;
+}
+
