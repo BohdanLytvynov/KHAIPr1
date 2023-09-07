f63c595a9335a0ac49fa3a8b02f49e963a395eb2
BohdanLytvynov <blitvinov19@gmail.com>
2023-09-07 20:37:27 +0300

    Task 3 finished

---
+++ Task3/Task3.cpp
@@ -0,0 +1,20 @@
+﻿// Task3.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include <windows.h>
+
+int main()
+{
+    HANDLE console = GetStdHandle(STD_OUTPUT_HANDLE);
+
+    SetConsoleTextAttribute(console, FOREGROUND_RED | BACKGROUND_BLUE);
+
+    std::cout << "Hello World!\n";
+
+    int getch();
+
+    return 0;
+}
+
+
